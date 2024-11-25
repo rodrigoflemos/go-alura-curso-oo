d10fe02711caf2a234f503b70c5d72201bda708b
Rodrigo Lemos <rodrigoflemos@gmail.com>
2024-11-25 19:21:27 -0300

    feat: :sparkles: create-withdrawal-method

--- main.go
+++ main.go
@@ -9,41 +9,29 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+
+	if valorDoSaque < 0 {
+		return "O valor do saque precisa ser positivo"
+	}
+	podeSacar := valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 	contaDoRodrigo := ContaCorrente{
 		titular:       "Rodrigo",
 		numeroAgencia: 589,
 		numeroConta:   123456,
-		saldo:         125.50,
+		saldo:         500,
 	}
-
-	contaDoRodrigo2 := ContaCorrente{
-		titular:       "Rodrigo",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.50,
-	}
-
-	// == compara conteudo em Go
-	fmt.Println(contaDoRodrigo == contaDoRodrigo2)
-
-	//Outra maneira de inicializar struct
-	var contaDaJoana *ContaCorrente
-	contaDaJoana = new(ContaCorrente)
-	contaDaJoana.titular = "Joana"
-
-	var contaDaJoana2 *ContaCorrente
-	contaDaJoana2 = new(ContaCorrente)
-	contaDaJoana2.titular = "Joana"
-
-	fmt.Println(contaDaJoana)
-	fmt.Println(contaDaJoana2)
-	fmt.Println(&contaDaJoana)
-	fmt.Println(&contaDaJoana2)
-
-	//A comparacao é realizada por endereco de memoria = false
-	fmt.Println(contaDaJoana == contaDaJoana2)
-	//A comparacao é realizada por conteudo = true
-	fmt.Println(*contaDaJoana == *contaDaJoana2)
-
+	fmt.Println(contaDoRodrigo.saldo)
+	fmt.Println(contaDoRodrigo.Sacar(-100))
+	fmt.Println(contaDoRodrigo.saldo)
 }
